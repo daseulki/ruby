56329e75a8cd8d2a39afa0ce32767bb32a73d76a
daseulki <sisial1002@gmail.com>
2017-12-11 16:42:44 +0900

    random game

--- week2/app.rb
+++ week2/app.rb
@@ -41,18 +41,37 @@ get '/square/:num' do
   erb :square
 
 end
-
-
 get '/search' do
 
   erb :search
 
 end
-
 get '/qresult' do
 
   @q = params[:q]
   @name = params[:name]
   erb :qresult
+
+end
+get '/lunch' do
+  @lunch = ["없어","밥 없어","공기"].sample
+  @img_url = {
+      "없어" =>"http://cfile1.uf.tistory.com/image/2041714E50EE665F0D9593",
+      "밥 없어" =>"http://cfile1.uf.tistory.com/image/2041714E50EE665F0D9593",
+      "공기" =>"http://cfile1.uf.tistory.com/image/2041714E50EE665F0D9593"
+    }
+  erb :lunch
+end
+get '/game' do
+
+  erb :game
+
+end
+
+get '/gresult' do
+
+  @modifier = ["장난아닌", "슈퍼파워","울트라","갓","킹","초특급","밀레니엄","캡숑","짱","캡틴","오지고지리고렛잇고"].to_a.sample
+  @gname = params[:gname]
+  erb :gresult
 
 end
