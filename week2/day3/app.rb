f504ab138c874f57390fd02b3a3e98e64fd38098
daseulki <sisial1002@gmail.com>
2017-12-13 14:41:08 +0900

    아이디 중복, 비밀번호 확인

--- week2/day3/app.rb
+++ week2/day3/app.rb
@@ -16,22 +16,47 @@ end
 get '/signin' do
   erb :signin
 end
-get '/idfail' do
-  erb :idfail
-end
+
 get '/idcomplete' do
   @id = params[:id]
   @pwd = params[:pwd]
   @pwdconfig = params[:pwdconfig]
+  idconfig = true
+
+
+  CSV.foreach('user.csv',encoding: 'utf-8') do |row|
+    if row[0] == @id
+      idconfig = false
+      break
+    else
+      idconfig = true
+    end
+  end
+
+  if idconfig == false
+    redirect '/idfail'
+  else
+    erb :idcomplete
+  end
+
+
 
   if @pwd == @pwdconfig
     CSV.open('user.csv','a+:utf-8') do |csv|
       csv << [@id,@pwd]
     end
   else
-    redirect '/idfail'
+    redirect '/pwfail'
   end
   erb :idcomplete
+end
+
+get '/idfail' do
+  erb :idfail
+end
+
+get '/pwfail' do
+  erb :pwfail
 end
 
 get '/post' do
